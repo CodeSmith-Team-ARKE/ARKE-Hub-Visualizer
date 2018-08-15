2ab9f7cbc6ef7a0d72cfc729e3eca4f721576475
budken <siu.afmm@gmail.com>
2018-08-15 11:34:27 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-east-2' });
+
+// console.log(AWS.config);
+const cloudwatch = new AWS.CloudWatch();
+
+// logs the current S3 buckets
+// var s3 = new AWS.S3();
+// s3.listBuckets({}, function(err, data) {
+//   if (err) console.log(err, err.stack);
+//   else console.log(data);
+// });
+
+var params = {
+  DashboardName: 'Test-Board' /* required */
+};
+cloudwatch.getDashboard(params, function(err, data) {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else console.log(data); // successful response
+});
+
+// < =================================================== > //
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+console.log('Production build running on ' + 8080);
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to the AWS: Team ARKE Project</h1>
+        </header>
+        <div className="contents Ec2-reference">
+          {' '}
+          Some kind of relevant information here, more more and more Data Some
+          kind of relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more DataSome kind of
+          relevant information here, more more and more Data
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
